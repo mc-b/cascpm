83f0fd1964d2749d3beed1ecc3a1cf8daf875a05
mc-b <marcel.bernet@ch-open.ch>
2024-08-22 17:50:13 +0200

    Komment raus

--- provider.tf
+++ provider.tf
@@ -16,7 +16,7 @@ terraform {
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
-  #access_key = ""
-  #secret_key = ""
-  #token      = ""
+  access_key = ""
+  secret_key = ""
+  token      = ""
 }
