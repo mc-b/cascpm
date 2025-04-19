fcdd0d82447e50fe766e88632c5319a29897a540
mc-b <marcel.bernet@mc-b.ch>
2025-04-19 09:53:42 +0200

    Optimierungen

--- join.tf
+++ join.tf
@@ -1,4 +1,6 @@
 
+# Join Cluster
+
 resource "local_file" "join_script" {
   count = terraform.workspace != "lernmaas" ? 1 : 0
 
@@ -12,33 +14,32 @@ resource "local_file" "join_script" {
   file_permission = "0755"
 }
 
-# Join Cluster
-
 locals {
   is_lernmaas = terraform.workspace == "lernmaas"
+
+  target_host = local.is_lernmaas ? "localhost" : try(module.vms.fqdn_vm["controlplane-01"], "localhost")
 }
 
 resource "null_resource" "join_cluster" {
   count = local.is_lernmaas ? 0 : 1
 
-  depends_on = [local_file.join_script]
+  depends_on = [
+    local_file.join_script
+  ]
 
   provisioner "local-exec" {
     interpreter = ["bash", "-c"]
 
     command = <<EOT
-%{if !local.is_lernmaas}
-  echo "[+] Warte auf SSH-Verfügbarkeit auf der Controlplane..."
-  sleep 60
-  for i in {1..120}; do
-    ssh -o BatchMode=yes -o ConnectTimeout=5 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/lerncloud ubuntu@${local.controlplane_fqdn} "echo 'SSH OK'" && break || echo "SSH noch nicht verfügbar, versuche erneut..." && sleep 10
-  done
-  echo "[+] SSH Verbindung steht, kopiere join.sh..."
-  scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/lerncloud join.sh ubuntu@${local.controlplane_fqdn}:join.sh
-%{else}
-  echo "Skip join_cluster because workspace is lernmaas"
-%{endif}
+      echo "[+] Warte auf SSH-Verfügbarkeit auf der Controlplane..."
+      sleep 60
+      for i in {1..120}; do
+        ssh -o BatchMode=yes -o ConnectTimeout=5 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/lerncloud ubuntu@${local.target_host} "echo 'SSH OK'" && break || echo "SSH noch nicht verfügbar, versuche erneut..." && sleep 10
+      done
+      echo "[+] SSH Verbindung steht, kopiere join.sh..."
+      scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/lerncloud join.sh ubuntu@${local.target_host}:join.sh
 EOT
   }
 }
+
 
